6cef24864838bde981c7c4b2b87cb224db044f29
sukjun2 <tjrwns3650@gmail.com>
2022-12-15 11:18:23 +0900

    1215

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -12,23 +12,66 @@ function raf(time) {
 requestAnimationFrame(raf);
 // lenis 효과
 
-const loader = document.querySelector(".loading");
-const html = document.querySelector("html");
+//loading
+function loading() {
+    console.log("loading");
+    let progress = $(".progress"),
+        progressText = progress.find(".progress-text"),
+        imgLoad = imagesLoaded("body"),
+        imgTotal = imgLoad.images.length,
+        imgLoaded = 0,
+        imgCurrent = 0,
+        progressTimer = setInterval(updateProgress, 1000 / 60);
+    html = document.querySelector("html");
 
-html.style.overflow = "hidden"; //로딩 중 스크롤 방지
+    imgLoad.on("progress", function () {
+        imgLoaded++;
+        html.style.overflow = "hidden";
+    });
 
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        //로딩속도 구현
+    function updateProgress() {
+        let target = (imgLoaded / imgTotal) * 100;
 
-        loader.style.opacity = "0";
-        html.style.overflow = "auto"; //스크롤 방지 해제
+        imgCurrent += (target - imgCurrent) * 0.1;
+        progressText.text(Math.floor(imgCurrent) + "%");
 
-        setTimeout(() => {
-            loader.style.display = "none";
-        }, 400);
-    }, 5000);
-});
+        if (imgCurrent >= 100) {
+            clearInterval(progressTimer);
+            progress.delay(1000).animate(
+                {
+                    top: "-130%",
+                },
+                400
+            );
+
+            const tl = new TimelineLite({ delay: 3 });
+            tl.to(".header__menu", {
+                delay: -1,
+                xPercent: 100,
+                opacity: 1,
+                duration: 1,
+            })
+                .set(".header__menu ul li", {
+                    opacity: 0,
+                })
+                .to(".header__menu ul li", {
+                    y: 0,
+                    opacity: 1,
+                    duration: 1,
+                    stagger: 0.2,
+                    ease: "bounce.out",
+                });
+            // header
+        }
+
+        if (imgCurrent > 99) {
+            imgCurrent = 100;
+            html.style.overflow = "auto";
+        }
+    }
+}
+loading();
+
 // 로딩
 
 window.addEventListener("scroll", () => {
@@ -147,27 +190,6 @@ gsap.to("#progress", {
 });
 // 메뉴바 프로그래스
 
-window.onload = function () {
-    const tl = new TimelineLite({ delay: 7 });
-    tl.to(".header__menu", {
-        delay: -1,
-        xPercent: 100,
-        opacity: 1,
-        duration: 1,
-    })
-        .set(".header__menu ul li", {
-            opacity: 0,
-        })
-        .to(".header__menu ul li", {
-            y: 0,
-            opacity: 1,
-            duration: 1,
-            stagger: 0.2,
-            ease: "bounce.out",
-        });
-};
-// header
-
 // 글씨 쪼개기(다중이)
 document.querySelectorAll(".split").forEach((text) => {
     let splitText = text.innerText;
@@ -180,7 +202,7 @@ document.querySelectorAll(".split").forEach((text) => {
 gsap.from(".title span", {
     yPercent: -300,
     scale: 0,
-    delay: 8.3,
+    delay: 3.3,
     opacity: 0,
     duration: 2,
     stagger: 0.1,
@@ -197,7 +219,7 @@ gsap.to(".title span", {
 // section01
 
 gsap.from(".rocket", {
-    delay: 10,
+    delay: 5,
     opacity: 0,
 });
 gsap.from(".rocket", {
@@ -211,7 +233,7 @@ gsap.from(".rocket", {
     },
 });
 gsap.from(".about__me", {
-    delay: 10.5,
+    delay: 5.5,
     opacity: 0,
 });
 gsap.from(".profile01", {
@@ -342,7 +364,7 @@ ScrollTrigger.create({
 });
 gsap.from(".bubble__effect01", {
     opacity: 0,
-    delay: 9,
+    delay: 4,
 });
 gsap.to(
     [
